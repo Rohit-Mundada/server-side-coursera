04d449de448fa17f13c160a6be5384c8d54a874d
Rohit-Mundada <rohitrm0801@gmail.com>
2020-05-22 23:03:36 +0530

    Express session Part 2

--- app.js
+++ app.js
@@ -47,44 +47,22 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    let authHeader = req.headers.authorization;
-
-    if (!authHeader) {
-      let err = new Error("You are not authenticated!");
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    let auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-
-    let user = auth[0];
-    let pass = auth[1];
-
-    if (user === "admin" && pass === "password") {
-      req.session.user = "admin";
-      next(); // authorized
-    } else {
-      let err = new Error("You are not authenticated!");
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     } else {
-      let err = new Error("You are not authenticated!");
-
-      err.status = 401;
+      var err = new Error("You are not authenticated!");
+      err.status = 403;
       return next(err);
     }
   }
@@ -94,8 +72,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
